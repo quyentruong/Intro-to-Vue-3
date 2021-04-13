0908f42571e335b9de5d95c5a1f1b4b72261cc19
Quyen Truong <quyen.truong@codesmartinc.com>
2021-04-13 10:48:45 -0700

    done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
         }
     }
 })
